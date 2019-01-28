63c72bab549ae2c5aaa6370aebe10cce1e14effe
Endre Bakken Stovner <endrebak85@gmail.com>
2019-01-28 12:58:55 +0100

    Add version flag to sorted nearest

--- sorted_nearest/__init__.py
+++ sorted_nearest/__init__.py
@@ -2,3 +2,6 @@ from sorted_nearest.src.sorted_nearest import (nearest_previous_nonoverlapping,
                                                nearest_next_nonoverlapping,
                                                nearest_nonoverlapping,
                                                find_clusters)
+
+
+from sorted_nearest.version import __version__
