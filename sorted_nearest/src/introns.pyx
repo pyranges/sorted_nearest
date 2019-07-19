da60073f5987f35436a2f79a8cbe3b87f2415674
Endre Bakken Stovner <endrebak85@gmail.com>
2019-07-19 15:09:02 +0200

    Hotfix

--- sorted_nearest/src/introns.pyx
+++ sorted_nearest/src/introns.pyx
@@ -5,9 +5,6 @@ cimport cython
 
 import numpy as np
 
-import pyranges as pr
-import pandas as pd
-
 def find_introns(gene_starts, gene_ends, gene_ids,
                  exon_starts, exon_ends, exon_gene_ids):
 
@@ -19,6 +16,9 @@ def find_introns(gene_starts, gene_ends, gene_ids,
         raise Exception("Starts/Ends not int64 or int32: " + str(gene_starts.dtype))
 
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.initializedcheck(False)
 cpdef find_introns64(const long [::1] gene_starts, const long [::1] gene_ends, const long [::1] gene_ids,
                      const long [::1] exon_starts, const long [::1] exon_ends, const long [::1] x_gene_ids):
 
@@ -73,6 +73,9 @@ cpdef find_introns64(const long [::1] gene_starts, const long [::1] gene_ends, c
     return output_arr_start[:n], output_arr_end[:n], output_arr_id[:n]
 
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.initializedcheck(False)
 cpdef find_introns32(const int32_t [::1] gene_starts, const int32_t [::1] gene_ends, const long [::1] gene_ids,
                      const int32_t [::1] exon_starts, const int32_t [::1] exon_ends, const long [::1] x_gene_ids):
 
@@ -100,8 +103,23 @@ cpdef find_introns32(const int32_t [::1] gene_starts, const int32_t [::1] gene_e
         gene_id = gene_ids[i]
         gene_start = gene_starts[i]
         gene_end = gene_ends[i]
+        # print("-----" * 5)
+        # print("gene_id", gene_id)
+        # print("gene_start", gene_start)
+        # print("gene_end", gene_end)
+        # print("j", j)
+        # print()
+        # assert gene_ids[i] == x_gene_ids[j], "gene_exon_not_equal"
+
+        # print("gene_start != exon_starts[j]", gene_start != exon_starts[j])
+
+        # assert gene_start <= exon_starts[j], "gene_start not smaller or equal to exon_end: {}, {}".format(gene_start, exon_starts[j])
 
         if gene_start != exon_starts[j]:
+            # print("exon_start != gene_start")
+            # print("insert start", gene_start)
+            # print("insert end", exon_starts[j])
+            # print()
             output_start[n] = gene_start
             output_end[n] = exon_starts[j]
             output_id[n] = gene_id
@@ -109,7 +127,17 @@ cpdef find_introns32(const int32_t [::1] gene_starts, const int32_t [::1] gene_e
 
         j += 1
 
+        # print("gene_start != exon_starts[j]", gene_start != exon_starts[j])
+        # print("j", j)
+        # if j < exon_length:
+        #     print("gene_id == x_gene_ids[j]", gene_id == x_gene_ids[j], gene_id, x_gene_ids[j])
         while j < exon_length and gene_id == x_gene_ids[j]:
+            # if exon_ends[j - 1] == 1217804:
+            #     print(gene_id)
+            #     raise
+            # print("insert start", exon_ends[j - 1])
+            # print("insert end", exon_starts[j])
+            # print()
 
             output_start[n] = exon_ends[j - 1]
             output_end[n] = exon_starts[j]
@@ -117,7 +145,14 @@ cpdef find_introns32(const int32_t [::1] gene_starts, const int32_t [::1] gene_e
             n += 1
             j += 1
 
+        # if j < exon_length:
+            # print("while ended here:", gene_id, x_gene_ids[j])
+
         if exon_ends[j - 1] != gene_end:
+            # print("exon_ends[j - 1] != gene_end", exon_ends[j - 1] != gene_end)
+            # print("insert start", exon_ends[j - 1])
+            # print("insert end", gene_end)
+            # print()
 
             output_start[n] = exon_ends[j - 1]
             output_end[n] = gene_end
